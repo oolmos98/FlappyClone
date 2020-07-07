deb206fa829fe99055b9a3deb29e1f98c99082ac
Omar Olmos <oolmos@ucmerced.edu>
2020-07-06 21:12:51 -0700

    Camera follows the ball tester

--- FlappyClone/FlappyClone/GameViewController.swift
+++ FlappyClone/FlappyClone/GameViewController.swift
@@ -59,7 +59,7 @@ class GameViewController: UIViewController {
         sceneView = (self.view as! SCNView)
         
         sceneView.showsStatistics = true
-        sceneView.allowsCameraControl = true
+        sceneView.allowsCameraControl = false
         sceneView.autoenablesDefaultLighting = true
     }
     
@@ -69,7 +69,8 @@ class GameViewController: UIViewController {
         
         sceneView.scene = sceneScene
         
-        sceneView.backgroundColor = UIColor.cyan
+        sceneView.delegate = self
+
         
         let tapRecognizer = UITapGestureRecognizer()
         tapRecognizer.numberOfTouchesRequired = 1
@@ -88,8 +89,7 @@ class GameViewController: UIViewController {
         cameraNode.camera = SCNCamera()
         
         // Assign its position in the scene
-        cameraNode.position = SCNVector3(x: 0, y: 2, z: 20)
-        
+        cameraNode.position = SCNVector3(x: 0, y: 2, z: 5)
         // Add camera node to child of scene root node
         sceneScene.rootNode.addChildNode(cameraNode)
     }
@@ -100,17 +100,57 @@ class GameViewController: UIViewController {
     }
     
     @objc func sceneViewTapped (recognizer:UITapGestureRecognizer) {
-        let location = recognizer.location(in: sceneView)
-        let hitResults = sceneView.hitTest(location, options: nil)
-        
-        if(hitResults.count > 0){
-            let result = hitResults.first
-            if let node = result?.node{
-                if node.name == "ball"{
-                    bird.birdNode!.physicsBody?.applyForce(SCNVector3(x: 0.2, y: 1, z: 0), asImpulse: true)
-                }
-            }
-        }
+//        let location = recognizer.location(in: sceneView)
+//        let hitResults = sceneView.hitTest(location, options: nil)
+//
+//        if(hitResults.count > 0){
+//            let result = hitResults.first
+//            if let node = result?.node{
+//                if node.name == "ball"{
+//                    bird.birdNode!.physicsBody?.applyForce(SCNVector3(x: 0, y: 1.5, z: 0), asImpulse: true)
+//                }
+//            }
+//        }
+        bird.birdNode!.physicsBody?.applyForce(SCNVector3(x: 0, y: 1.5, z: -0.5), asImpulse: true)
+
     }
     
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+}
+
+
+
+
+//Render function. what to render based on sudden changes such as moving the camera if an object moves.
+extension GameViewController : SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+        //Camera follows the ball.
+        //Referenced https://github.com/brianadvent/HitTheTree/blob/master/HitTheTree/GameViewController.swift
+        
+       let ball = bird.birdNode!.presentation
+       let ballPosition = ball.position
+       
+       let targetPosition = SCNVector3(x: ballPosition.x, y: ballPosition.y + 2, z:ballPosition.z + 15)
+       var cameraPosition = cameraNode.position
+       
+       let camDamping:Float = 0.3
+       
+       let xComponent = (cameraPosition.x * (1 - camDamping)) + (targetPosition.x * camDamping)
+       let yComponent = (cameraPosition.y * (1 - camDamping)) + (targetPosition.y * camDamping)
+       let zComponent = (cameraPosition.z * (1 - camDamping)) + (targetPosition.z * camDamping)
+       
+       cameraPosition = SCNVector3(x: xComponent, y: yComponent, z: zComponent)
+
+        cameraNode.position = cameraPosition
+       // bird.birdNode!.physicsBody?.applyForce(SCNVector3(x: 0, y: 0.2, z: 0), asImpulse: true)
+        print(cameraNode.position)
+        
+    }
 }
